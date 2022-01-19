e826c743d4a4d84027f7bf4dc9eb389f674da5ef
Roman Plotnikov <rekss13@gmail.com>
2022-01-19 12:11:12 +0300

    cleared the warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useState} from "react";
 import ThemeContext from "./context/ThemeContext";
 import AppTheme from "./AppTheme";
 import Header from "./components/Header";
